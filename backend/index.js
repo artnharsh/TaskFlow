690eb327be943c31fb764ce405799ddfb7bd5deb
artnharsh <artnharsh@gmail.com>
2026-07-17 22:32:32 +0530

    cors edited

--- backend/index.js
+++ backend/index.js
@@ -14,11 +14,25 @@ const {initSocket} = require("../backend/src/socket/index")
 
 const app = express();
 
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://task-flow.vercel.app",
+    "https://task-flow-4uepquzz0-xcalibers-projects.vercel.app",
+];
+
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL || "https://task-flow.vercel.app",
+        origin: function (origin, callback) {
+            if (!origin) return callback(null, true);
+
+            if (allowedOrigins.includes(origin)) {
+                return callback(null, true);
+            }
+
+            callback(new Error("Not allowed by CORS"));
+        },
         credentials: true,
-    }),
+    })
 );
 
 app.use(express.json({ limit: '1mb' }));
