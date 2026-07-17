5fa42e02db5e11449778b08a43d5708364d5d4ef
artnharsh <artnharsh@gmail.com>
2026-07-17 22:35:56 +0530

    cors changed to all temp

--- backend/index.js
+++ backend/index.js
@@ -21,18 +21,10 @@ const allowedOrigins = [
 ];
 
 app.use(
-    cors({
-        origin: function (origin, callback) {
-            if (!origin) return callback(null, true);
-
-            if (allowedOrigins.includes(origin)) {
-                return callback(null, true);
-            }
-
-            callback(new Error("Not allowed by CORS"));
-        },
-        credentials: true,
-    })
+  cors({
+    origin: true,
+    credentials: true,
+  })
 );
 
 app.use(express.json({ limit: '1mb' }));
